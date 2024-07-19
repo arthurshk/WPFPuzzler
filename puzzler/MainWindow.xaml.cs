68e0b48cb5a1f6173492f6e9f85646f8c6c8d009
Arthur <arthurdragon39@gmail.com>
2024-07-19 00:02:51 -0400

    commit

--- puzzler/MainWindow.xaml.cs
+++ puzzler/MainWindow.xaml.cs
@@ -1,17 +1,14 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media.Imaging;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace puzzler
 {
     public partial class MainWindow : Window
     {
-        private BitmapImage uploadedImage;
-
         public MainWindow()
         {
             InitializeComponent();
@@ -19,42 +16,43 @@ namespace puzzler
 
         private void UploadPicture_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Filter = "Image Files (*.png; *.jpg)|*.png;*.jpg";
+
+            bool? result = dlg.ShowDialog();
+
+            if (result == true)
             {
-                Filter = "Image files (*.png;*.jpg)|*.png;*.jpg"
-            };
-            if (openFileDialog.ShowDialog() == true)
-            {
-                uploadedImage = new BitmapImage(new Uri(openFileDialog.FileName));
-                CreatePuzzle(uploadedImage);
+                string filename = dlg.FileName;
+                BitmapImage bitmap = new BitmapImage(new Uri(filename));
+                LoadPuzzlePieces(bitmap);
             }
         }
 
-        private void CreatePuzzle(BitmapImage image)
+        private void LoadPuzzlePieces(BitmapImage bitmap)
         {
-            int rows = 4; 
-            int cols = 4; 
-            double pieceWidth = image.PixelWidth / cols;
-            double pieceHeight = image.PixelHeight / rows;
+            int rows = 4;
+            int cols = 4;
+            double pieceWidth = bitmap.PixelWidth / cols;
+            double pieceHeight = bitmap.PixelHeight / rows;
 
             PiecesPanel.Children.Clear();
-            PuzzleArea.Children.Clear();
 
-            for (int row = 0; row < rows; row++)
+            for (int i = 0; i < rows; i++)
             {
-                for (int col = 0; col < cols; col++)
+                for (int j = 0; j < cols; j++)
                 {
-                    CroppedBitmap piece = new CroppedBitmap(image, new Int32Rect((int)(col * pieceWidth), (int)(row * pieceHeight), (int)pieceWidth, (int)pieceHeight));
-                    Image pieceImage = new Image
+                    CroppedBitmap croppedBitmap = new CroppedBitmap(bitmap, new Int32Rect(j * (int)pieceWidth, i * (int)pieceHeight, (int)pieceWidth, (int)pieceHeight));
+                    Image piece = new Image
                     {
-                        Source = piece,
                         Width = pieceWidth,
                         Height = pieceHeight,
-                        Tag = new Point(col, row) 
+                        Source = croppedBitmap,
+                        Tag = new Point(j, i),
+                        Margin = new Thickness(5)
                     };
-
-                    pieceImage.MouseDown += Piece_MouseDown;
-                    PiecesPanel.Children.Add(pieceImage);
+                    piece.MouseDown += Piece_MouseDown;
+                    PiecesPanel.Children.Add(piece);
                 }
             }
         }
@@ -62,27 +60,38 @@ namespace puzzler
         private void Piece_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Image piece = sender as Image;
-            DataObject data = new DataObject(piece);
-            DragDrop.DoDragDrop(piece, data, DragDropEffects.Move);
+            if (piece != null)
+            {
+                DataObject data = new DataObject(typeof(Image), piece);
+                DragDrop.DoDragDrop(piece, data, DragDropEffects.Move);
+            }
         }
 
         private void PuzzleArea_Drop(object sender, DragEventArgs e)
         {
-            Image piece = e.Data.GetData(typeof(Image)) as Image;
-            if (piece != null)
+            if (e.Data.GetDataPresent(typeof(Image)))
             {
-                Point dropPosition = e.GetPosition(PuzzleArea);
-                Canvas.SetLeft(piece, dropPosition.X - piece.Width / 2);
-                Canvas.SetTop(piece, dropPosition.Y - piece.Height / 2);
-                PuzzleArea.Children.Add(piece);
-                PiecesPanel.Children.Remove(piece);
+                Image piece = e.Data.GetData(typeof(Image)) as Image;
+                if (piece != null)
+                {
+                    if (piece.Parent is Panel oldParent)
+                    {
+                        oldParent.Children.Remove(piece);
+                    }
+                    Point dropPosition = e.GetPosition(PuzzleArea);
+                    Canvas.SetLeft(piece, dropPosition.X - piece.Width / 2);
+                    Canvas.SetTop(piece, dropPosition.Y - piece.Height / 2);
+                    PuzzleArea.Children.Add(piece);
 
-                CheckPuzzleSolved();
+                    CheckPuzzleSolved();
+                }
             }
         }
 
         private void CheckPuzzleSolved()
         {
+            bool puzzleSolved = true;
+
             foreach (UIElement child in PuzzleArea.Children)
             {
                 if (child is Image piece)
@@ -95,12 +104,16 @@ namespace puzzler
 
                     if (Math.Abs(correctX - currentPosition.X) > piece.Width / 10 || Math.Abs(correctY - currentPosition.Y) > piece.Height / 10)
                     {
-                        return; 
+                        puzzleSolved = false;
+                        break;
                     }
                 }
             }
 
-            MessageBox.Show("Puzzle Solved!");
+            if (puzzleSolved)
+            {
+                MessageBox.Show("Puzzle Solved!");
+            }
         }
     }
 }
