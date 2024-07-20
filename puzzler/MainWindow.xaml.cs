c39386434131b17b54170d873d13f33641757e1b
Arthur <arthurdragon39@gmail.com>
2024-07-20 15:03:54 -0400

    commit

--- puzzler/MainWindow.xaml.cs
+++ puzzler/MainWindow.xaml.cs
@@ -9,6 +9,10 @@ namespace puzzler
 {
     public partial class MainWindow : Window
     {
+        private Point _startPoint;
+        private Image _draggedPiece;
+        private bool _isDragging;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -51,40 +55,58 @@ namespace puzzler
                         Tag = new Point(j, i),
                         Margin = new Thickness(5)
                     };
-                    piece.MouseDown += Piece_MouseDown;
+                    piece.MouseLeftButtonDown += Piece_MouseLeftButtonDown;
+                    piece.MouseMove += Piece_MouseMove;
+                    piece.MouseLeftButtonUp += Piece_MouseLeftButtonUp;
                     PiecesPanel.Children.Add(piece);
                 }
             }
         }
 
-        private void Piece_MouseDown(object sender, MouseButtonEventArgs e)
+        private void Piece_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            Image piece = sender as Image;
-            if (piece != null)
+            _draggedPiece = sender as Image;
+            if (_draggedPiece != null)
             {
-                DataObject data = new DataObject(typeof(Image), piece);
-                DragDrop.DoDragDrop(piece, data, DragDropEffects.Move);
+                _startPoint = e.GetPosition(PuzzleArea);
+                _isDragging = true;
+                _draggedPiece.CaptureMouse();
             }
         }
 
-        private void PuzzleArea_Drop(object sender, DragEventArgs e)
+        private void Piece_MouseMove(object sender, MouseEventArgs e)
         {
-            if (e.Data.GetDataPresent(typeof(Image)))
+            if (_isDragging && _draggedPiece != null)
             {
-                Image piece = e.Data.GetData(typeof(Image)) as Image;
-                if (piece != null)
-                {
-                    if (piece.Parent is Panel oldParent)
-                    {
-                        oldParent.Children.Remove(piece);
-                    }
-                    Point dropPosition = e.GetPosition(PuzzleArea);
-                    Canvas.SetLeft(piece, dropPosition.X - piece.Width / 2);
-                    Canvas.SetTop(piece, dropPosition.Y - piece.Height / 2);
-                    PuzzleArea.Children.Add(piece);
+                Point currentPosition = e.GetPosition(PuzzleArea);
+                double offsetX = currentPosition.X - _startPoint.X;
+                double offsetY = currentPosition.Y - _startPoint.Y;
 
-                    CheckPuzzleSolved();
+                Canvas.SetLeft(_draggedPiece, Canvas.GetLeft(_draggedPiece) + offsetX);
+                Canvas.SetTop(_draggedPiece, Canvas.GetTop(_draggedPiece) + offsetY);
+
+                _startPoint = currentPosition;
+            }
+        }
+
+        private void Piece_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (_isDragging && _draggedPiece != null)
+            {
+                _draggedPiece.ReleaseMouseCapture();
+                _isDragging = false;
+
+                Point dropPosition = e.GetPosition(PuzzleArea);
+                Canvas.SetLeft(_draggedPiece, dropPosition.X - _draggedPiece.Width / 2);
+                Canvas.SetTop(_draggedPiece, dropPosition.Y - _draggedPiece.Height / 2);
+
+                if (_draggedPiece.Parent is Panel oldParent)
+                {
+                    oldParent.Children.Remove(_draggedPiece);
                 }
+
+                PuzzleArea.Children.Add(_draggedPiece);
+                CheckPuzzleSolved();
             }
         }
 
